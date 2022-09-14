eff5e06370f1952f1eb0aea625712e4a1e789fce
Alejandro González Reyes <alexglzrys@gmail.com>
2022-09-14 16:43:57 -0500

    guardar y leer TODOS en LocalStorage - useReducer

--- src/components/08-useReducer/TodoApp.jsx
+++ src/components/08-useReducer/TodoApp.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useEffect } from 'react';
 import { useReducer } from 'react'
 import { TodoAdd } from './TodoAdd';
 import { TodoItem } from './TodoItem';
@@ -7,7 +8,7 @@ import { todoReducer } from './todoReducer';
 
 // Estado inicial
 const initialState = [
-    {
+    /*{
         id: new Date().getTime(),
         description: 'Correr 10km en el cerro',
         done: false
@@ -16,8 +17,18 @@ const initialState = [
         id: new Date().getTime() + 100,
         description: 'Aprender React 1 hora',
         done: false
-    }
+    }*/
 ]
+
+// Función inicializadora de estado para useReducer
+const init = () => {
+    // Como se están persistiendo los datos en LocalStorage, es necesario esta función inicializadora de estado,
+    // ya que de no hacerlo, cada vez que se recargue la página, el contenido almacenado en LocalStorage se eliminará
+    // esto es así ya que que cuando se carga el componente, se vuelve a inicializar el estado a un arreglo vacio
+    // -- ver initialState
+    // Para evitar ese comportamiento por defecto, inicializamos el estado de manera diferida, leerlo desde localStorage
+    return JSON.parse(localStorage.getItem('todos')) || [];
+}
 
 export const TodoApp = () => {
     /**
@@ -32,9 +43,23 @@ export const TodoApp = () => {
      * mismo que define la lógica y las posibles tareas a ejecutar.
      * 
      * Recibe como argumentos, la función Reducer (misma que debemos programar nosotros) y el estado inicial
+     * Opcionalmente recibe un tercer argumento, una función que permita inicializar el estado de forma diferida
      * Retorna el estado actual, y la función despachadora de acciones
      */
-    const [todos, dispatch] = useReducer(todoReducer, initialState);
+    const [todos, dispatch] = useReducer(todoReducer, initialState, init);
+
+    /**
+     * Persistir y leér estado en LocalStorage
+     * 
+     * Esto se puede considerar como un efecto secundario
+     * Se debe leér el estado desde el LocalStorage en el momento que se carga el componente
+     * y se debe guardar el estado en LocalStorage cada vez que este cambia (se agrega, actualiza, o elimina una tarea)
+     */
+    useEffect(() => {
+        // Persistir el estado cada vez que el arreglo de todos cambia
+      localStorage.setItem('todos', JSON.stringify(todos))
+    }, [todos])
+    
 
     const handleNewTodo = (todo) => {
         console.log({todo});
